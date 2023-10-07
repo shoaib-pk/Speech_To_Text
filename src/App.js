0c30c79aac5e7d0eab19085150f9e32a1a7fc99b
shoaib-pk <b-24080@student.usa.edu.pk>
2023-10-07 12:09:24 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css"
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+import useClipboard from "react-use-clipboard";
 
-function App() {
+const App = () => {
+  const [textToCopy,setTextToCopy]= useState();
+  const [isCopied, setCopied] = useClipboard(textToCopy);
+  const startListening= ()=> SpeechRecognition.startListening({ continuous: true, language:'en-IN' });
+  const { transcript, browserSupportsSpeechRecognition } = useSpeechRecognition();
+  if (!browserSupportsSpeechRecognition) {
+    return null
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <h2>Speech to Text Converter</h2>
+        <br />
+        <p>A React hook that is use there</p>
+
+        <div className="main-content" onClick={()=> setTextToCopy(transcript)}>
+        {transcript}
+        </div>
+        
+        <div className="btn-style">
+        <button onClick={setCopied}>
+          {isCopied? 'Copied': 'Copy to Clipboard'}
+       </button>
+        <button onClick={startListening}>start Linstening</button>
+        <button onClick={SpeechRecognition.stopListening}>Stop Linstening</button>
+        </div>
+      </div>
+
+    </>
   );
 }
 
